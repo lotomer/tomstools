b3b71882eee7c929ee54cb8f5163f1c08918447a
lotomer <lotomer@163.com>
2014-06-08 18:10:20 +0800

    添加遗留文件
    添加遗留文件

---
+++ code/LotteryPrediction/src/main/java/org/tomstools/prediction/lottery/LotteryWinResult.java
@@ -0,0 +1,15 @@
+/**
+ * copyright (a) 2010-2014 tomstools.org. All rights reserved.
+ */
+package org.tomstools.prediction.lottery;
+
+/**
+ * 彩票中奖情况
+ * @author admin
+ * @date 2014年6月8日 
+ * @time 下午5:55:38
+ * @version 1.0
+ */
+public class LotteryWinResult {
+
+}
